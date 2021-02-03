b23d4a836221ae0a4f1d3910f101755ba5bcf9a1
Angshuman Sengupta <senguptaangshuman17@gmail.com>
2021-02-03 18:43:25 +0530

    searchBook, viewBook and updateBook added and saveInfo renamed to addBook in BookManager.java

--- src/BookManager.java
+++ src/BookManager.java
@@ -17,8 +17,9 @@ import java.util.Scanner;
  * @author angshuman
  */
 public class BookManager {
+
     private String csvPath;
-    
+
     public BookManager(String csvPath) {
         this.csvPath = csvPath;
     }
@@ -49,7 +50,7 @@ public class BookManager {
         String tempFile = "temp.csv";
         File oldFile = new File(this.csvPath);
         File newFile = new File(tempFile);
-        String publisher = "",name = "",author = "",subject = "",isbn = "",price = "",picPath = "";
+        String publisher = "", name = "", author = "", subject = "", isbn = "", price = "", picPath = "";
         try {
             FileWriter fw = new FileWriter(tempFile, true);
             BufferedWriter bw = new BufferedWriter(fw);
@@ -65,8 +66,9 @@ public class BookManager {
                 isbn = x.next();
                 price = x.next();
                 picPath = x.next();
-                if (i != rownumber)
+                if (i != rownumber) {
                     pw.println(publisher + "," + name + "," + author + "," + subject + "," + isbn + "," + price + "," + picPath);
+                }
                 i++;
             }
             x.close();
@@ -81,7 +83,72 @@ public class BookManager {
 
     }
 
-    public void saveInfo(String inputString) {
+    public void updateBook(int row, String info) {
+        String arr[] = info.split(",");
+        String tempFile = "temp.csv";
+        File oldFile = new File(this.csvPath);
+        File newFile = new File(tempFile);
+        String publisher = "", name = "", author = "", subject = "", isbn = "", price = "", picPath = "";
+        try {
+            FileWriter fw = new FileWriter(tempFile, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter pw = new PrintWriter(bw);
+            Scanner x = new Scanner(new File(this.csvPath));
+            x.useDelimiter("[,\n]");
+            int i = 0;
+            while (x.hasNext()) {
+                publisher = x.next();
+                name = x.next();
+                author = x.next();
+                subject = x.next();
+                isbn = x.next();
+                price = x.next();
+                picPath = x.next();
+                if (i != row) {
+                    pw.println(publisher + "," + name + "," + author + "," + subject + "," + isbn + "," + price + "," + picPath);
+                } else {
+                    pw.println(arr[0] + "," + arr[1] + "," + arr[2] + "," + arr[3] + "," + arr[4] + "," + arr[5] + "," + arr[6]);
+                }
+                i++;
+            }
+            x.close();
+            pw.flush();
+            pw.close();
+            oldFile.delete();
+            File dump = new File(this.csvPath);
+            newFile.renameTo(dump);
+        } catch (Exception e) {
+            System.out.println("Error " + e);
+        }
+
+    }
+
+    public boolean searchBook(String bookName) {
+        File oldFile = new File(this.csvPath);
+        String publisher = "", name = "", author = "", subject = "", isbn = "", price = "", picPath = "";
+        try {
+            Scanner x = new Scanner(new File(this.csvPath));
+            x.useDelimiter("[,\n]");
+            while (x.hasNext()) {
+                publisher = x.next();
+                name = x.next();
+                author = x.next();
+                subject = x.next();
+                isbn = x.next();
+                price = x.next();
+                picPath = x.next();
+                if (name.equals(bookName)) {
+                    return true;
+                }
+            }
+            x.close();
+        } catch (Exception e) {
+            System.out.println("Error " + e);
+        }
+        return false;
+    }
+
+    public void addBook(String inputString) {
         try {
             FileWriter fw = new FileWriter(this.csvPath, true);
             BufferedWriter bw = new BufferedWriter(fw);
@@ -94,6 +161,39 @@ public class BookManager {
             pw.close();
         } catch (Exception E) {
             System.out.println("Error: " + E);
+        }
+    }
+
+    public void viewBook(String bookName) {
+        if (this.searchBook(bookName)) {
+//            String arr[] = info.split(",");
+//            String tempFile = "temp.csv";
+            File oldFile = new File(this.csvPath);
+//            File newFile = new File(tempFile);
+            String publisher = "", name = "", author = "", subject = "", isbn = "", price = "", picPath = "";
+            try {
+//                FileWriter fw = new FileWriter(tempFile, true);
+//                BufferedWriter bw = new BufferedWriter(fw);
+//                PrintWriter pw = new PrintWriter(bw);
+                Scanner x = new Scanner(new File(this.csvPath));
+                x.useDelimiter("[,\n]");
+                while (x.hasNext()) {
+                    publisher = x.next();
+                    name = x.next();
+                    author = x.next();
+                    subject = x.next();
+                    isbn = x.next();
+                    price = x.next();
+                    picPath = x.next();
+                    if (bookName.equalsIgnoreCase(name)) {
+                        System.out.println("Publisher: " + publisher + "\n" + "Name: " + name + "\n" + "Authir: " + author + "\n" + "Subject: " + subject + "\n" + "ISBN: " + isbn + "\n" + "Price: " + price + "\n" + "Picture Path: " + picPath);
+                        break;
+                    }
+                }
+                x.close();
+            } catch (Exception e) {
+                System.out.println("Error " + e);
+            }
         }
     }
 
