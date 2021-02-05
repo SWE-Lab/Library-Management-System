3eb7e13ed321ad749a5172981be9586b127249c3
HackMEAny <hackme.any@protonmail.com>
2021-02-06 03:22:36 +0530

    Member Entity completed

--- test/MemberManagerTest.java
+++ test/MemberManagerTest.java
@@ -17,42 +17,42 @@ public class MemberManagerTest {
 
         // Get total number of Members
         System.out.println(obj.getTotalMembers());
-//        assert obj.getTotalMembers() == 8 : "Wrong Total number of Member";
+        assert obj.getTotalMembers() == 8 : "Wrong Total number of Member";
         System.out.println("Total number of Member: " + obj.getTotalMembers());
 
 //        // Add new Member
         int oldNo = obj.getTotalMembers();
-//        obj.registerMember("Alpa,alpa@gm.com,6547892310,12345678,15963247854125,/home/angshuman/Pictures/Screenshot_20210104_110819.png,500");
-//        assert obj.getTotalMembers() == oldNo + 1 : "Member not Added";
-//        assert obj.getMember(oldNo).getName().equals("Alpa") : "Wrong Member added";
-//        System.out.println(obj.getMemberArr().get(oldNo).getName());
-//
+        obj.registerMember("Alpa,alpa@gm.com,6547892310,12345678,15963247854125,/home/angshuman/Pictures/Screenshot_20210104_110819.png,500");
+        assert obj.getTotalMembers() == oldNo + 1 : "Member not Added";
+        assert obj.getMember(oldNo).getName().equals("Alpa") : "Wrong Member added";
+        System.out.println(obj.getMemberArr().get(oldNo).getName());
+
 //        // Delete Member
-//        String oldPub = obj.getMember(7).getName();
-//        obj.deleteMember(7);
-//        String newPub = obj.getMember(7).getName();
-//        assert obj.getTotalMembers() == oldNo : "Wrong Index";
-//        assert !(oldPub.equals(newPub)) : "Not Deleted Proper Member";
+        String oldPub = obj.getMember(5).getName();
+        obj.deleteMember(5);
+        String newPub = obj.getMember(5).getName();
+        assert obj.getTotalMembers() == oldNo : "Wrong Index";
+        assert !(oldPub.equals(newPub)) : "Not Deleted Proper Member";
 
         // Update Member
-//        int index = 0;
-//        obj.updateMember(index, "Name", "AAAA");
-//        String newName = obj.getMember(index).getName();
-//        assert newName.equals("AAAA") : "Wrong Instance Updated";
+        int index = 0;
+        obj.updateMember(index, "Name", "AAAA");
+        String newName = obj.getMember(index).getName();
+        assert newName.equals("AAAA") : "Wrong Instance Updated";
 
         //Search Member
-//        Member member = obj.verifyMember("fgdg");
-//        assert member.getName().equals("fgdg") : "Wrong Search Result";
-//        System.out.println("Date of registration : " + member.getDate());
-//        System.out.println("Name : " + member.getName());
-//        System.out.println("Email : " + member.getEmail());
-//        System.out.println("Phone Number: " + member.getPhNo());
-//        System.out.println("Enroll No: " + member.getEnrollNo());
-//        System.out.println("Profile Pic: " + member.getPicPath());
-//        System.out.println("Fine: " + member.getFine());
+        Member member = obj.verifyMember("Debanjan Dhar");
+        assert member.getName().equals("Debanjan Dhar") : "Wrong Search Result";
+        System.out.println("Date of registration : " + member.getDate());
+        System.out.println("Name : " + member.getName());
+        System.out.println("Email : " + member.getEmail());
+        System.out.println("Phone Number: " + member.getPhNo());
+        System.out.println("Enroll No: " + member.getEnrollNo());
+        System.out.println("Profile Pic: " + member.getPicPath());
+        System.out.println("Fine: " + member.getFine());
 
         // Write back to json file
-//        obj.writeJSON();
+        obj.writeJSON();
 //        System.out.println("Update Book details & Search book");
     }
 }
